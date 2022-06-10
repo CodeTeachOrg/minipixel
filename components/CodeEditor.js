abad9735da6a3f931d2151a6ce98e29b919454f4
Cooper Saye <coopersaye@gmail.com>
2022-06-09 23:56:52 -0700

    Import ace editor.

--- components/CodeEditor.js
+++ components/CodeEditor.js
@@ -1,3 +1,12 @@
+import AceEditor from 'react-ace';
+
+import { useState } from 'react';
+
+import 'ace-builds/src-noconflict/mode-javascript.js';
+import 'ace-builds/src-noconflict/theme-github.js';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/ext-searchbox';
+
 import styles from '../styles/components/CodeEditor.module.css';
 
 export default function CodeEditor() {

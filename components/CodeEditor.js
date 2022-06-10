1b819098ad78eeff2bbb424703f7869b93a1d48e
Cooper Saye <coopersaye@gmail.com>
2022-06-09 23:57:07 -0700

    Create ace editor component.

--- components/CodeEditor.js
+++ components/CodeEditor.js
@@ -9,9 +9,24 @@ import 'ace-builds/src-noconflict/ext-searchbox';
 
 import styles from '../styles/components/CodeEditor.module.css';
 
-export default function CodeEditor() {
+export default function CodeEditor(props) {
+
   return (
-    <div>
+    <div className={styles.container}>
+      <AceEditor
+        mode="javascript"
+        theme="github"
+        wrapEnabled={true}
+        showPrintMargin={false}
+        tabSize={2}
+        setOptions={{
+          useWorker: false,
+          enableLiveAutocompletion: true
+        }}
+        width="500px"
+        height="500px"
+        {...props}
+      />
     </div>
   );
 }

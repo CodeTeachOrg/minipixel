457643243a77e7072b72c53775a6930b07003a3b
Cooper Saye <coopersaye@gmail.com>
2022-06-09 23:57:18 -0700

    Create code editor instance.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Code from '../components/Code';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
     <div>
+      <Code />
     </div>
   );
 }

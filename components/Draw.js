f88b390df73f5357ad572a7d4705e125784db2b1
Cooper Saye <coopersaye@gmail.com>
2022-06-14 23:13:24 -0700

    Initialize draw canvas.

--- components/Draw.js
+++ components/Draw.js
@@ -1,7 +1,16 @@
 import styles from '../styles/components/Draw.module.css';
 
+import { useEffect, useRef } from 'react';
+
+let canvas, ctx;
+
 export default function Draw() {
   const canvasRef = useRef();
+
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
 
   return (
     <div className={styles.container}>

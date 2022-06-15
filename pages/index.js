0a4210f9e70a2f268dc14f082117eb9a1fdf3497
Cooper Saye <coopersaye@gmail.com>
2022-06-14 23:12:04 -0700

    Set up engine layout.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Code from '../components/Code';
+import Draw from '../components/Draw';
+import Display from '../components/Display';
 
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
-    <div>
-      <Code />
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <Code />
+        <div>
+          <Draw />
+          <Display />
+        </div>
+      </div>
     </div>
   );
 }

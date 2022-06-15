63dee632f5fbd144575d8613bbcc18bce27327b9
Cooper Saye <coopersaye@gmail.com>
2022-06-14 23:14:26 -0700

    Wrap code in container.

--- components/Code.js
+++ components/Code.js
@@ -1,9 +1,15 @@
 import dynamic from 'next/dynamic';
 
+import styles from '../styles/components/Code.module.css';
+
 const CodeEditor = dynamic(import('./CodeEditor'), {
   loading: function Load() { return <p>Loading...</p> }, ssr: false
 });
 
 export default function Code(props) {
-  return <CodeEditor {...props} />;
+  return (
+    <div className={styles.container}>
+      <CodeEditor {...props} />
+    </div>
+  );
 }

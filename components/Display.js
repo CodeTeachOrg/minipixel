7f69ab91b84a8bf6f6e7fbe67900016477c27d04
Cooper Saye <coopersaye@gmail.com>
2022-06-14 23:13:35 -0700

    Create display canvas.

--- components/Display.js
+++ components/Display.js
@@ -1,8 +1,15 @@
 import styles from '../styles/components/Display.module.css';
 
 export default function Display() {
+  const canvasRef = useRef();
+
   return (
-    <div>
+    <div className={styles.container}>
+      <canvas
+        ref={canvasRef}
+        width="200px"
+        height="200px"
+      />
     </div>
   );
 }

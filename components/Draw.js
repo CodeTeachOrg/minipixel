5cf756e90ef8a9d1ae14b45b696262745eca65f7
Cooper Saye <coopersaye@gmail.com>
2022-06-14 23:13:16 -0700

    Create draw canvas.

--- components/Draw.js
+++ components/Draw.js
@@ -1,8 +1,15 @@
 import styles from '../styles/components/Draw.module.css';
 
 export default function Draw() {
+  const canvasRef = useRef();
+
   return (
-    <div>
+    <div className={styles.container}>
+      <canvas
+        ref={canvasRef}
+        width="200px"
+        height="200px"
+      />
     </div>
   );
 }

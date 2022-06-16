641f5a1bb0c0f51155f98149cca7d641ba862070
Cooper Saye <coopersaye@gmail.com>
2022-06-15 23:18:28 -0700

    Make code controlled.

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,20 @@ import Code from '../components/Code';
 import Draw from '../components/Draw';
 import Display from '../components/Display';
 
+import { useState } from 'react';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
+  const [code, setCode] = useState('');
+
   return (
     <div className={styles.container}>
       <div className={styles.center}>
-        <Code />
+        <Code
+          value={code}
+          onChange={val => setCode(val)}
+        />
         <div>
           <Draw />
           <Display />
